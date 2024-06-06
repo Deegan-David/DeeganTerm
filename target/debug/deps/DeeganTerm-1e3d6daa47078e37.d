75184e9ecac8767975d93fdc0f4731fd20431c19
Deegan David <deegandavid06@icloud.com>
2024-06-06 10:05:00 -0400

    Hello, Git!

---
+++ target/debug/deps/DeeganTerm-1e3d6daa47078e37.d
@@ -0,0 +1,5 @@
+/home/deegan/Documents/rust-projects/DeeganTerm/target/debug/deps/DeeganTerm-1e3d6daa47078e37: src/main.rs
+
+/home/deegan/Documents/rust-projects/DeeganTerm/target/debug/deps/DeeganTerm-1e3d6daa47078e37.d: src/main.rs
+
+src/main.rs:
